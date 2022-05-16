d04ea875e20336c4903d70af9adc6f8751fdde85
Carlap09 <cpacheco0978@gmail.com>
2022-05-16 00:02:19 -0300

    se agrego los componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { SobreMiComponent } from './componentes/sobre-mi/sobre-mi.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    SobreMiComponent,
+    EducacionComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    AptitudesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
